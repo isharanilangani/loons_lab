93e33a1a5ed7d7c22bf793c9a8fc2daa47b4cb00
isharanilangani <98374135+isharanilangani@users.noreply.github.com>
2025-05-08 15:54:32 +0530

    feat: set up routing for main pages using React Router

--- movie-explorer/src/App.js
+++ movie-explorer/src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import MovieDetails from './pages/MovieDetails';
+import { MovieProvider } from './context/MovieContext';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <MovieProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </Router>
+    </MovieProvider>
+  );
 }
 
 export default App;
